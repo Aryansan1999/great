c9228f065b335633445fba2d330b4f8a2bab60cf
Aryan <2005152@kiit.ac.in>
2022-02-28 20:22:12 +0530

    final greet

--- app/src/main/java/com/example/wish/Rakshabandhan.java
+++ app/src/main/java/com/example/wish/Rakshabandhan.java
@@ -3,36 +3,24 @@ package com.example.wish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
-import org.w3c.dom.Text;
-
-public class last extends AppCompatActivity {
-
+public class Rakshabandhan extends AppCompatActivity {
+    LottieAnimationView animationView;
     TextView textView1;
     TextView textView2;
-    LottieAnimationView sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_last);
-        sp = findViewById(R.id.splas);
+        setContentView(R.layout.activity_rakshabandhan);
+        animationView=findViewById(R.id.frame);
         textView1= findViewById(R.id.name);
         textView2= findViewById(R.id.namewish);
-
-
         String name1 = getIntent().getStringExtra("text1");
         String wish = getIntent().getStringExtra("text2");
-
-
         textView1.setText(name1);
         textView2.setText(wish);
-
-
     }
-
 }
--- app/src/main/java/com/example/wish/input.java
+++ app/src/main/java/com/example/wish/input.java
@@ -17,7 +17,7 @@ public class input extends AppCompatActivity {
     EditText name, mess;
     Spinner spinner;
     Button button;
-
+public int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +26,6 @@ public class input extends AppCompatActivity {
         button = findViewById(R.id.button);
         name = findViewById(R.id.inpname);
         mess = findViewById(R.id.mess);
-
-        int i = 0;
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.fest, simple_spinner_item);
         adapter.setDropDownViewResource(simple_spinner_item);
@@ -40,11 +38,59 @@ public class input extends AppCompatActivity {
                 String text = spinner.getSelectedItem().toString();
                 String text1 = name.getText().toString();
                 String text2 = mess.getText().toString();
-                Intent intent = new Intent(getApplicationContext(), last.class);
-                intent.putExtra("text1", text1);
-                intent.putExtra("text2", text2);
-                intent.putExtra("text", text);
-                startActivity(intent);
+                if(text.equals("Holi"))
+                    i=1;
+                else if(text.equals("Diwali"))
+                    i=2;
+                else if(text.equals("Rakshabandhan"))
+                    i=3;
+                else if(text.equals("Birthday"))
+                    i=4;
+                else if(text.equals("Anniversary"))
+                    i=5;
+
+                switch (i){
+                    case 1:
+                    {
+                        Intent intent = new Intent(getApplicationContext(), last.class);
+                        intent.putExtra("text1", text1);
+                        intent.putExtra("text2", text2);
+                        startActivity(intent);
+                        break;
+                    }
+                    case 2:
+                    {
+                        Intent intent = new Intent(getApplicationContext(), diwali.class);
+                        intent.putExtra("text1", text1);
+                        intent.putExtra("text2", text2);
+                        startActivity(intent);
+                        break;
+                    }
+                    case 3:
+                    {
+                        Intent intent = new Intent(getApplicationContext(), Rakshabandhan.class);
+                        intent.putExtra("text1", text1);
+                        intent.putExtra("text2", text2);
+                        startActivity(intent);
+                        break;
+                    }
+                    case 4:
+                    {
+                        Intent intent = new Intent(getApplicationContext(), Birthday.class);
+                        intent.putExtra("text1", text1);
+                        intent.putExtra("text2", text2);
+                        startActivity(intent);
+                        break;
+                    }
+                    case 5:
+                    {
+                        Intent intent = new Intent(getApplicationContext(), Anniversary.class);
+                        intent.putExtra("text1", text1);
+                        intent.putExtra("text2", text2);
+                        startActivity(intent);
+                        break;
+                    }
+                }
             }
         });
     }
